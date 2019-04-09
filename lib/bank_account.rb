e5c0bb187d42da01196c233782aff78000be3fea
CLARE PINDER <CLARE.PINDER@LIVE.CO.UK>
2019-04-09 11:42:32 +0100

    passed test: BankAccount withdrawals and dates are added to statement

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -10,4 +10,8 @@ class BankAccount
   def deposit(value, date = Time.new.strftime('%d/%m/%Y'))
     @statement << { date: date, credit: value }
   end
+
+  def withdraw(value, date = Time.new.strftime('%d/%m/%Y'))
+    @statement << { date: date, debit: value }
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -11,8 +11,15 @@ describe 'BankAccount' do
     expect(@account.statement).to eq []
   end
 
-  it 'deposit and date are added to statement' do
+  it 'deposits and date are added to statement' do
     @account.deposit(1000, date_double)
     expect(@account.statement).to eq [{ date: date_double, credit: 1000 }]
+  end
+
+  it 'withdrawals and dates are added to statement' do
+    @account.deposit(1000, date_double)
+    @account.withdraw(500, date_double)
+    expect(@account.statement).to eq [{ date: date_double, credit: 1000 },
+                                      { date: date_double, debit: 500 }]
   end
 end
